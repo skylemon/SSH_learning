98ffce13ca693727ffcfb238c199c40f68f13cf1
lemon su <249032041@qq.com>
2018-02-26 21:55:09 +0800

    struts2

---
+++ SSH_Struts/src/com/susl/HelloWorldAction.java
@@ -0,0 +1,21 @@
+package com.susl;
+
+public class HelloWorldAction {
+	private String name;
+
+	public String execute() throws Exception {
+
+		if (getName().equals("") || getName() == null)
+			return "error";
+
+		return "success";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
