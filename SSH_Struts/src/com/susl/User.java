7895211f272429b72c80c87295fcfb2ba5d8a635
lemon su <249032041@qq.com>
2018-03-14 23:01:24 +0800

    asdfasdf

---
+++ SSH_Struts/src/com/susl/User.java
@@ -0,0 +1,55 @@
+package com.susl;
+
+public class User {
+	private String name;
+    private String password;
+    private String confirmPassword;
+    private int age;
+    private String mobile;
+    private String email;
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getMobile() {
+        return mobile;
+    }
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override  
+    public String toString() {  
+        return "User： name=" + name + ", password=" + password  
+                + ", confirm password=" + confirmPassword + ", age=" + age  
+                + ", mobile=" + mobile + ", email=" + email ;  
+    }
+}
---
+++ SSH_Struts/src/com/susl/ValidateAction.java
@@ -0,0 +1,18 @@
+package com.susl;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ValidateAction extends ActionSupport {
+	private static final long serialVersionUID = 1L;
+	private User user;
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public String execute(){
+		return "success";
+	}
+}
